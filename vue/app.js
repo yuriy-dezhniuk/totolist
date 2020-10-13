c76d627345a145bcc67bc7b8012c174ed1c01239
yuriy-dezhniuk <33997334+yuriy-dezhniuk@users.noreply.github.com>
2020-10-13 17:44:16 +0300

    Add style

--- vue/app.js
+++ vue/app.js
@@ -1,41 +1,51 @@
 let vm = new Vue({
-  el: '#todo-list',
+  el: '#app',
   data: {
     newTodoText: '',
-    todos: [
-      // {
-      //   id: 1,
-      //   title: 'Помыть посуду',
-      //   stage: false,
-      // },
-      // {
-      //   id: 2,
-      //   title: 'Вынести мусор',
-      //   stage: false,
-      // },
-      // {
-      //   id: 3,
-      //   title: 'Подстричь газон',
-      //   stage: false,
-      // }
-    ],
-    nextTodoId: new Date() - Math.random(),
-    // "ID-"+(new Date()).getTime().toString()
+    todos: JSON.parse(localStorage.getItem('todos')),
+    // todos: [
+    //   // {
+    //   //   id: 1,
+    //   //   title: 'Помыть посуду',
+    //   //   stage: true,
+    //   // },
+    //   // {
+    //   //   id: 2,
+    //   //   title: 'Вынести мусор',
+    //   //   stage: false,
+    //   // },
+    //   // {
+    //   //   id: 3,
+    //   //   title: 'Подстричь газон',
+    //   //   stage: false,
+    //   // }
+    // ],
+    // nextTodoId: 0,
   },
   methods: {
+    generateTodoId() {
+      return '_' + (new Date - Math.random()).toString(36).substr(1, 9)
+    },
+    updateToDoState(updatedStage, todoIndex) {
+      this.todos[todoIndex].stage = updatedStage;
+      this.sendDataToLS();
+    },
     addNewTodo: function () {
       if(this.newTodoText === '') {
         alert('Task should not be empty')
       } else {
         this.todos.unshift({
-          id: this.nextTodoId++,
+          id: this.generateTodoId(),
           title: this.newTodoText,
           stage: false,
         });
-        console.log(this.todos[0].id);
+        this.sendDataToLS();
         this.newTodoText = '';
       }
     },
+    sendDataToLS() {
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+    },   
   },
 
 
--- vue/components.js
+++ vue/components.js
@@ -1,16 +1,22 @@
 Vue.component('todo-item', {
-  template: '\
-    <li>\
-      <input type="checkbox" name="scales"\
-        v-on:change="$emit(\'change\', stage ? stage = false : stage = true )">\
-      <span v-bind:class="{done: stage, undone: !stage}"></span>\
-      {{ title }}  {{ stage }}\
-      <button v-on:click="$emit(\'remove\')">Удалить</button>\
-    </li>\
-  ',
+  template: `
+    <li class="list-item">
+      <div>
+        <input type="checkbox" name="scales" :checked="stage" v-on:change="changeStage">
+        <span class="indicator" v-bind:class="{done: stage}"></span>
+        <span v-bind:class="{linethrough: stage}">
+          {{ title }}
+        </span>
+      </div>
+      <button class="btn delete-item-btn" v-on:click="$emit('remove')">Delete</button>
+    </li>
+  `,
   props: ['title', 'stage',],
-  model: {
-    event: 'change',
+  methods: {
+    changeStage() {
+      this.$emit('ubdatestate', !this.stage);
+    },
   },
+
 });
 
